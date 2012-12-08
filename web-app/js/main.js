b9dfc72d98221e68d57e3f0d683b43b98de5f08a
e.pirogov <e.pirogov@itech-group.ru>
2012-12-09 01:44:45 +0400

    - basic frontend functionality for auth/registration

--- web-app/js/main.js
+++ web-app/js/main.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 
     initScrollTop();
 
+    initHiddenForms();
+
 });
 
 initScrollTop = function () {
@@ -19,9 +21,29 @@ initScrollTop = function () {
     });
 
     $('.scrollTop a').click(function () {
-        $('body').animate({
+        $('body, html').animate({
             scrollTop: 0
         }, 1000);
+        return false;
+    });
+};
+
+initHiddenForms = function () {
+    $(document).on('click', 'div.authorization-container a', function () {
+        var victimSelector = $(this).attr('href'),
+            $victimSource = $(this).next(victimSelector),
+            $victimTarget = $('.body-wrapper');
+        if ($victimSource.length) {
+            $victimSource
+                .attr('id', victimSelector.replace('#','_'))
+                .clone()
+                    .attr('id', victimSelector.replace('_', ''))
+                    .insertAfter($victimTarget)
+                    .removeClass('hidden')
+                    .hide()
+                    .fadeIn();
+
+        };
         return false;
     });
 };
